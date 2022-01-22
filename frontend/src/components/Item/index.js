25dfbf0893b996d50a26c086b838507da634158e
Oryam <48219965+or-yam@users.noreply.github.com>
2022-01-22 22:26:43 +0200

    Update mobile view layout

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -48,7 +48,7 @@ class Item extends React.Component {
       <div className="container page">
         <div className="text-dark">
           <div className="row bg-white p-4">
-            <div className="col-6">
+            <div className="d-none col-6">
               <img
                 src={this.props.item.image || '/placeholder.png'}
                 alt={this.props.item.title}
