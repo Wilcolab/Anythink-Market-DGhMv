601630aa08ab35e46f1728dbd7df64ed79d09bcf
Oryam <48219965+or-yam@users.noreply.github.com>
2022-02-16 16:44:48 +0200

    Update nodejs version

--- backend/app.js
+++ backend/app.js
@@ -93,8 +93,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-const PORT = process.env.PORT || 3000;
 // finally, let's start our server...
-var server = app.listen(PORT, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port " + server.address().port);
 });
