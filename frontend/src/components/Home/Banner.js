c190af01d4d22cc65e4b159fd7e6a8d2a6369c0d
Oryam <48219965+or-yam@users.noreply.github.com>
2022-04-09 16:51:33 +0300

    Implement Search component inside Banner

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import Search from "./Search";
 
 const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img width="100%" src={logo} alt="banner" />
-        <h2>A place to get the cool stuff.</h2>
+        <h2>
+          <span>A place to get</span>
+          <Search />
+          <span>the cool stuff.</span>
+        </h2>
       </div>
     </div>
   );
