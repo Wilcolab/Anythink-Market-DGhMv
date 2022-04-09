7b7c8564289864a94368d91bdb8674559815b89b
Oryam <48219965+or-yam@users.noreply.github.com>
2022-04-09 20:04:17 +0300

    Move search state to index file Move search imput to banner

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,19 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import Search from "./Search";
 
-const Banner = () => {
+const Banner = (props) => {
+  const handleSearchChange = e => {
+    //? remove spaces
+    props.setSearchText(e.target.value);
+  };
+  
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img width="100%" src={logo} alt="banner" />
         <h2>
           <span>A place to get</span>
-          <Search />
+          <input id="search-box" type="text" placeholder="Search" onChange={handleSearchChange} />
           <span>the cool stuff.</span>
         </h2>
       </div>
